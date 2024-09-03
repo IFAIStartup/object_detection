45d2f37fe146aa9bbe756a1009b71a42c06ae63b
IFAIStartup <ifaistartup@gmail.com>
2024-09-03 11:11:14 +0530

    bug solve

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def predict_img():
                 if filename=='image.png':
                     flash("Image upload sucessfully",'success')
                 model=YOLO('best.pt')
-                results=model.predict(filepath, save=True ,project="static", name="predict", exist_ok=True,show=True)
+                results=model.predict(filepath, save=True ,project="static", name="predict", exist_ok=True)
                 filename='predict'+'/'+filename
                 counts={}
                 for results in results:
