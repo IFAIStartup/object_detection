437e02d6898fcda0b5bc48d7d12e7b4902d6940a
IFAIStartup <ifaistartup@gmail.com>
2024-09-03 20:05:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,11 +49,11 @@ def predict_img():
                                 counts[cls]=1
                             else:
                                 counts[cls]+=1
-                       label=[]
-                       count=[]
-                       for key in counts.keys():
-                           label.append(model.names[key])
-                           count.append(str(counts[key]))
+                        label=[]
+                        count=[]
+                        for key in counts.keys():
+                            label.append(model.names[key])
+                            count.append(str(counts[key]))
 
                     return render_template('index.html', filename=predict_filename,label=label,count=count)
                 except Exception as e:
