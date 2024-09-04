f04cfd50f3fbbecc5227ca9db6417c71d2350106
IFAIStartup <ifaistartup@gmail.com>
2024-09-04 23:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ os.makedirs(app.config['PREDICT_FOLDER'], exist_ok=True)
 
 
 
-model = YOLO('yolov5nu.pt')
+model = YOLO('yolov8n.pt')
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'mp4'}
